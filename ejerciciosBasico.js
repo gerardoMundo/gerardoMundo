012f12c3bdeb3e83ff48e48a640f1b76ef10051c
Gerardo <ger.mundo66@gmail.com>
2022-07-19 23:16:38 -0500

    segundo ejeercicio

---
+++ ejerciciosBasico.js
@@ -0,0 +1,10 @@
+const miNombre = "Gerardo Mundo";
+const edad = 34;
+const desarrollador = false;
+const nacimiento = new Date(1988, 2, 21);
+const libroFavorito = {
+    titulo: "El senior de los anillos",
+    autor: "JRR Tolkien",
+    fecha: "1945",
+    url: "https://www.amazon.com.mx/El-se%C3%B1or-los-anillos-I/dp/8445073729"
+}
--- index.js
+++ index.js
@@ -1,2 +1,5 @@
 console.log("Hola mundo");
 console.log("Desde Open Bootcamp");
+
+const fecha = new Date();
+console.log(fecha);
