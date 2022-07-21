8bb1e1366de790779309186e6dab7b68ed9940fb
Gerardo Mundo <ger.mundo66@gmail.com>
2022-07-20 19:59:47 -0500

    ejercicios strings

---
+++ ejerciciosStrs.js
@@ -0,0 +1,10 @@
+const nombre = "Gerardo";
+const apellido = "Mundo";
+const estudiante = `${nombre} ${apellido}`;
+const estudianteMayus = estudiante.toUpperCase();
+const estudianteMinus = estudiante.toLowerCase();
+const numeroLetras = estudiante.length;
+const primeraLetra = nombre.startsWith();
+const ultimaLetra = apellido.endsWith();
+const sinEspacios = estudiante.trim();
+const incluye = estudiante.includes(nombre);
