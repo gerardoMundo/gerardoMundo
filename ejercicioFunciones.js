129863b8ec20d977367e3b7d7c30673321dde91f
Gerardo <ger.mundo66@gmail.com>
2022-07-21 20:51:42 -0500

    ejercicio func

---
+++ ejercicioFunciones.js
@@ -0,0 +1,42 @@
+const { Error } = require("@mui/icons-material");
+
+const siempreTrue = () => {
+    return true;
+};
+
+const saludo = "Hola, soy una promesa";
+
+const temporizador = () => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve(saludo);
+        }, 5000);
+    });
+}
+
+temporizador().then(saludo => console.log(saludo));
+
+function* generarId() {
+    
+    let id = 0;
+    
+    while(id <= 10) {
+        id += 2;
+        if(id === 10) {
+            return id;
+        }
+        yield id;    
+    };
+};
+
+const generador = generarId();
+
+console.log(generador.next());
+console.log(generador.next());
+console.log(generador.next());
+
+
+
+
+
+
